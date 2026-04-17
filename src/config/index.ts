4c34678321adbd5c67e8a4b62debdd7b8fa5f86f
Md. Kobirul Islam <kobirul05j@gmail.com>
2026-04-17 20:59:56 +0600

    remove unwanted code

--- src/config/index.ts
+++ src/config/index.ts
@@ -27,15 +27,7 @@ const config = {
     reset_pass_secret: process.env.RESET_PASS_TOKEN,
     reset_pass_token_expires_in: process.env.RESET_PASS_TOKEN_EXPIRES_IN,
   },
-  reset_pass_link: process.env.RESET_PASS_LINK,
-  emailSender: {
-    email: process.env.EMAIL,
-    app_pass: process.env.APP_PASS,
-    smtp_server: process.env.SMTP_SERVER || process.env.smtp_server,
-    smtp_port: process.env.SMTP_PORT || process.env.smtp_port,
-    smtp_user: process.env.SMTP_USER || process.env.smtp_user,
-    smtp_pass: process.env.SMTP_PASS || process.env.smtp_pass,
-  },
+
   brevoMail: {
     api_key: process.env.BREVO_API_KEY,
     api_secret: process.env.BREVO_API_SECRET,
@@ -49,23 +41,6 @@ const config = {
     url:
       process.env.CLOUDINARY_URL ||
       process.env.ENVIRONMENT_VARIABLE?.replace("CLOUDINARY_URL=", ""),
-  },
-  stripe: {
-    publishable_key: process.env.STRIPE_PUBLISHABLE_KEY,
-    secret_key: process.env.STRIPE_SECRET_KEY,
-    webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
-    admin_account: process.env.STRIPE_ADMIN_ACCOUNT_ID,
-  },
-  platformCharge: {
-    percentage: Number(process.env.PLATFORM_CHARGE_PERCENTAGE) || 10,
-  },
-  client: {
-    url: clientUrl,
-  },
-  twilio: {
-    accountSid: process.env.TWILIO_ACCOUNT_SID,
-    authToken: process.env.TWILIO_AUTH_TOKEN,
-    twilioPhoneNumber: process.env.TWILIO_PHONE_NUMBER,
   },
   hadith: {
     apiBaseUrl:
